8ac781af96a3a5bacf11998c1b03db2ed58a9e25
ezekiel <ezekiel.xavier@yahoo.com>
2018-12-04 00:56:32 -0600

    Added test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
@@ -8,11 +8,26 @@ def hello_world():
 
 @app.route('/hi')
 def hi_there():
-    
+
     jsy ={
         'field1' :'yello',
         'field2':'bloue'
     }
     return jsonify(jsy)
+
+
+@app.route('/add',methods=['POST'])
+def addTwoNumbers():
+    dataDict = request.get_json()
+    if 'x' and 'y' not in dataDict:
+        return "ERROR" ,305
+    x = dataDict['x']
+    y = dataDict['y']
+    z = x + y
+
+    ret_json = {'z':z}
+
+    return jsonify(ret_json),200
+
 if __name__ == "__main__":
     app.run(debug=True)
